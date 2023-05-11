9fd9d3c9e86b972b612efff20fc4177365de95a1
AndrePonce3322 <andreponce417@gmail.com>
2023-05-10 23:35:35 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// HTTP Client
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+const enviroment = {
+    production: false,
+    URL_LOGIN: 'http://localhost:2000/db/login',
+}
