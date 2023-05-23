abc29af8b1b5426a8c858e59a282a13a3cf8da27
AndrePonce3322 <andreponce417@gmail.com>
2023-05-23 09:35:34 -0600

    modified

---
+++ src/app/Components/notes/home/home.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataBaseService } from 'src/app/Services/database.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+
+  FormTitleContent!: FormGroup;
+
+  constructor(private DBS: DataBaseService, private fb: FormBuilder) {
+
+    this.FormTitleContent = this.fb.group({
+      title: [''],
+      content: ['', [Validators.required]],
+      important: [false],
+      favorite: [false],
+    });
+
+    this.getUser();
+  }
+
+  DataUser = {
+    userName: '',
+    user: '',
+  };
+
+  ngOnInit(): void {
+    const textTarea = document.querySelector('textarea') as HTMLTextAreaElement;
+    const join = document.querySelector('.join') as HTMLTextAreaElement;
+
+    textTarea.addEventListener('input', () => {
+      textTarea.style.height = 'auto';
+      textTarea.style.height = `${textTarea.scrollHeight}px`;
+    });
+
+    document.addEventListener('click', (event) => {
+      if (!join.contains(event.target as Node)) {
+        this.showForm = false;
+      }
+    });
+  }
+
+  UserNotes: any = [];
+
+  getUser() {
+    this.DBS.Get().subscribe((data) => {
+      this.DataUser.userName = data[0].userName;
+      this.DataUser.user = data[0].user;
+      data[0].notes.map((notes) => {
+        this.UserNotes.push(notes);
+      });
+    });
+  }
+
+  showForm = false;
+  ShowForm() {
+    this.showForm = true;
+  }
+
+  Send(){
+    const textTarea = document.querySelector('textarea') as HTMLTextAreaElement;
+    textTarea.style.height = 'auto';
+
+    console.log(this.FormTitleContent.value);
+    this.FormTitleContent.reset();
+  }
+
+}
--- src/app/Components/notes/notes.module.ts
+++ src/app/Components/notes/notes.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SidebarMenuComponent } from '../sidebar-menu/sidebar-menu.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
 import { NotesRoutingModule } from './notes-routing.module';
 import { NotesComponent } from './notes.component';
-import { SidebarMenuComponent } from '../sidebar-menu/sidebar-menu.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { CardComponent } from './card/card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [NotesComponent, SidebarMenuComponent, CardComponent],
-  imports: [CommonModule, NotesRoutingModule, SharedModule],
+  declarations: [
+    NotesComponent,
+    SidebarMenuComponent,
+    CardComponent,
+    HomeComponent,
+  ],
+  imports: [CommonModule, NotesRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class NotesModule {}
