64e750493848593547d2fc0e89c2eeda44b8dde2
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2023-09-12 19:50:23 -0600

    New domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
     production: true,
-    URL_LOGIN: 'https://app-nodejs-mongodb-3e7e2be5385b.herokuapp.com/db/login',
-    URL_NOTES: 'https://app-nodejs-mongodb-3e7e2be5385b.herokuapp.com/db/notes',
-    URL_USERS: 'https://app-nodejs-mongodb-3e7e2be5385b.herokuapp.com/db/users',
-    URL_VERIFY: 'https://app-nodejs-mongodb-3e7e2be5385b.herokuapp.com/db/verify'
+    URL_LOGIN: 'https://notenexus.zeabur.app/db/login',
+    URL_NOTES: 'https://notenexus.zeabur.app/db/notes',
+    URL_USERS: 'https://notenexus.zeabur.app/db/users',
+    URL_VERIFY: 'https://notenexus.zeabur.app/db/verify'
 }
